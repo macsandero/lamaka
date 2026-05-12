dda861c1b58f5f86dffe8aad2655e9a934fa12c3
Alessandro Munzi <macsandero@gmail.com>
2026-05-12 18:07:12 +0200

    Update Lamaka map coordinates

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -90,7 +90,7 @@ class DatabaseSeeder extends Seeder
                 'phone' => null,
                 'whatsapp' => null,
                 'address' => 'Cadore - Dolomiti',
-                'map_query' => 'LAMAKA 46.4460542,12.3917181',
+                'map_query' => 'LAMAKA 46.446076,12.391663',
                 'instagram_url' => null,
                 'booking_label' => 'Prenota',
                 'booking_url' => null,
